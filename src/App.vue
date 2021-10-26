d795c4aa96f502e0389fad8ae9a611bc0b00eb3d
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-10-26 02:55:49 -0300

    refatorção da página inicial concluída

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,65 @@
 <template>
-  <div class="container">
-    <div class="wrapper">
-      <header>
-        <img :src="astronaut" class="header-astro" alt="Astronauta" />
-        <h2 class="title">Perguntas frequentes</h2>
-        <h3 class="subtitle">Escolha a categoria desejada</h3>
-      </header>
-      <main>
-        <ul class="faq-categories">
-          <li
-            class="list_items"
-            v-for="categorie in faqCategories"
-            :key="categorie.name"
-            @click="currentView = categorie.name"
-          >
-            <img :src="categorie.image" :alt="categorie.name" />
-            {{ categorie.name }}
-          </li>
-        </ul>
-      </main>
+    <div class="container">
+        <div class="wrapper">
+            <header>
+                <img :src="astronaut" class="header-astro" alt="Astronauta" />
+                <h2 class="title">Perguntas frequentes</h2>
+                <h3 class="subtitle">Escolha a categoria desejada</h3>
+            </header>
+            <main>
+                <ul class="faq-categories">
+                    <li
+                        class="list_items"
+                        v-for="categorie in $allQuestions"
+                        :key="categorie.id"
+                        @click="currentView = categorie.title"
+                    >
+                        <Logo :logoName="categorie.icon" />
+                        {{ categorie.title }}
+                    </li>
+                </ul>
+            </main>
+        </div>
     </div>
-  </div>
 
-  <component :is="currentView" />
+    <component :is="currentView" />
 </template>
 
 <script>
 import astronaut from "@/assets/images/astronaut.svg";
-import basecampIcon from "@/assets/icons/rocket.svg";
-import bootcampIcon from "@/assets/icons/astronaut-helmet.svg";
-import catalineIcon from "@/assets/icons/student-hat.svg";
-import partneshipIcon from "@/assets/icons/partnership.svg";
 
 import Basecamp from "@/pages/Basecamp.vue";
 import Bootcamp from "@/pages/Bootcamp.vue";
 import Cataline from "@/pages/Cataline.vue";
 import Parcerias from "@/pages/Partnership.vue";
+import Logo from "./components/Logo.vue";
 
 export default {
-  components: { Basecamp, Bootcamp, Cataline, Parcerias },
+    components: { Basecamp, Bootcamp, Cataline, Parcerias, Logo },
 
-  setup() {
-    return {
-      astronaut,
-      basecampIcon,
-      bootcampIcon,
-      catalineIcon,
-      partneshipIcon,
-    };
-  },
-  data() {
-    return {
-      faqCategories: [
-        { name: "Basecamp", image: basecampIcon },
-        { name: "Bootcamp", image: bootcampIcon },
-        { name: "Cataline", image: catalineIcon },
-        { name: "Parcerias", image: partneshipIcon },
-      ],
-      currentView: "Home",
-    };
-  },
-  computed: {
-    $allQuestions() {
-      return this.$store.getters.$allQuestions;
+    setup() {
+        return {
+            astronaut,
+        };
     },
-  },
-  created() {
-    this.$store.dispatch("fetchAllQuestions");
-  },
+    data() {
+        return {
+            currentView: "Home",
+        };
+    },
+    computed: {
+        $allQuestions() {
+            return this.$store.getters.$allQuestions;
+        },
+    },
+    created() {
+        this.$store.dispatch("fetchAllQuestions");
+    },
 };
 </script>
 
 <style>
 header {
-  padding: 1rem;
+    padding: 1rem;
 }
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,23 +1,19 @@
 <template>
-  <img :src="require(`@/assets/icons/${logoName}`)" :alt="logoName" />
+    <img :src="require(`@/assets/icons/${logoName}`)" :alt="logoName" />
 </template>
 
 <script>
-// import basecampIcon from "@/assets/icons/rocket.svg";
-// import bootcampIcon from "@/assets/icons/astronaut-helmet.svg";
-// import catalineIcon from "@/assets/icons/student-hat.svg";
-// import partneshipIcon from "@/assets/icons/partnership.svg";
 export default {
-  props: {
-    logoName: {
-      type: String,
-      required: true,
+    props: {
+        logoName: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  data() {
-    return {};
-  },
-  computed: {},
+    data() {
+        return {};
+    },
+    computed: {},
 };
 </script>
 
