323f3ea077815ad058b165257876e26fba365e9a
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-10-27 23:44:48 -0300

    inicio da fase3

---
+++ src/pages/Answers.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        <h3 class="subtitle">{{ categorieName }}</h3>
+        <p class="content">{{ content }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        categorieName: {
+            type: String,
+            default: "Nome",
+        },
+        content: {
+            type: String,
+            default: "conteúdo",
+        },
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/Basecamp.vue
+++ src/pages/Basecamp.vue
@@ -1,47 +1,59 @@
 <template>
-  <div class="wrapper">
-    <Header :categorieName="$categorieName.title" :icon="$categorieName.icon" />
-    <main>
-      <ul class="faq-categories">
-        <li
-          class="list_items"
-          v-for="categorie in $basecampQuestions"
-          :key="categorie.id"
-        >
-          {{ categorie.title }}
-        </li>
-      </ul>
-    </main>
-  </div>
+    <div class="wrapper">
+        <Header
+            :categorieName="$categorieName.title"
+            :icon="$categorieName.icon"
+        />
+        <main>
+            <ul class="faq-categories">
+                <li
+                    class="list_items"
+                    v-for="categorie in $basecampQuestions"
+                    :key="categorie.id"
+                    @click="currentView = 'Answers'"
+                >
+                    {{ categorie.title }}
+                </li>
+            </ul>
+        </main>
+    </div>
+    <component
+        :is="currentView"
+        :categorieName="$categorieName.title"
+        :content="$categorieName.content"
+    />
 </template>
 
 <script>
 import basecampIcon from "@/assets/icons/rocket.svg";
 import backArrow from "@/assets/icons/backArrow.svg";
 import Header from "../components/Header.vue";
+import Answers from "./Answers.vue";
 
 export default {
-  components: { Header },
-  data() {
-    return {
-      basecampIcon,
-      backArrow,
-    };
-  },
-  computed: {
-    $basecampQuestions() {
-      return this.$store.getters.$basecampQuestions;
+    components: { Header, Answers },
+    data() {
+        return {
+            basecampIcon,
+            backArrow,
+            currentView: "Home",
+        };
     },
-    $categorieName() {
-      const { title, icon } = this.$store.getters.$allQuestions.find(
-        (cat) => cat.title === "Basecamp"
-      );
-      return { title, icon };
+    computed: {
+        $basecampQuestions() {
+            return this.$store.getters.$basecampQuestions;
+        },
+        $categorieName() {
+            const { title, icon } = this.$store.getters.$allQuestions.find(
+                (cat) => cat.title === "Basecamp"
+            );
+            return { title, icon };
+        },
     },
-  },
-  created() {
-    this.$store.dispatch("fetchBasecampQuestion");
-  },
+    created() {
+        this.$store.dispatch("fetchBasecampQuestion");
+        console.log(this.$basecampQuestions);
+    },
 };
 </script>
 
