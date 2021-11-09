ee233e5f8c438df2860e5643a7c6e1034343fc8b
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-11-08 23:16:51 -0300

    adicionado estilo do app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="app">
-    <div class="container">
-      <transition class="slider" :name="transitionType" mode="out-in">
-        <component :is="$currentComponent" />
-      </transition>
+    <div class="app">
+        <div class="container">
+            <transition class="slider" :name="transitionType" mode="out-in">
+                <component :is="$currentComponent" />
+            </transition>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -14,37 +14,52 @@ import Questions from "@/components/Questions";
 import Answer from "@/components/Answer";
 
 export default {
-  name: "App",
-  components: { FaqCategories, Questions, Answer },
-
-  data() {
-    return {
-      transitionType: "slide-left",
-    };
-  },
-
-  computed: {
-    $currentComponent() {
-      return this.$store.getters.$currentComponent;
+    name: "App",
+    components: { FaqCategories, Questions, Answer },
+    data() {
+        return {
+            transitionType: "slide-left",
+        };
     },
-    $transitionDepth() {
-      return this.$store.getters.$transitionDepth;
+    computed: {
+        $currentComponent() {
+            return this.$store.getters.$currentComponent;
+        },
+        $transitionDepth() {
+            return this.$store.getters.$transitionDepth;
+        },
     },
-  },
-  watch: {
-    $transitionDepth(newValue, oldValue) {
-      if (newValue > oldValue) {
-        this.transitionType = "slide-left";
-      } else {
-        this.transitionType = "slide-right";
-      }
+    watch: {
+        $transitionDepth(newValue, oldValue) {
+            if (newValue > oldValue) {
+                this.transitionType = "slide-left";
+            } else {
+                this.transitionType = "slide-right";
+            }
+        },
     },
-  },
 };
 </script>
 
-<style>
-header {
-  padding: 1rem;
+<style lang="scss">
+.app {
+    display: grid;
+    grid-template-columns: 305px;
+    grid-template-rows: 29.389rem;
+    justify-content: center;
+    align-content: center;
+    height: 100vh;
+    background: #26282c;
+    .container {
+        padding: 2rem 0.375rem;
+        padding-bottom: 0.875rem;
+        background-image: linear-gradient(to bottom, #3f4452, #26282c);
+        border-radius: 15px;
+        -webkit-box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+        -moz-box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+        box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+        overflow: hidden;
+        position: relative;
+    }
 }
 </style>
